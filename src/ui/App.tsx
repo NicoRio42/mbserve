c03469ef42850b5ff103535af594f3ad6e97a648
Nicolas Rio <nicolas.rio42@gmail.com>
2026-03-05 09:47:34 +0100

    Remove maxBounds

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -35,7 +35,6 @@ export default function App() {
       mapStyle={mapStyle}
       hash
       maxPitch={85}
-      maxBounds={config.bounds}
     >
       <NavigationControl />
       {supportsTerrainPreview ? (
